01a2a530a2c5391fac06bd19ae63dd383966d455
Alexandre LUDWIG <ludwig.alexandre@laposte.net>
2019-09-10 12:56:08 +0200

    add project

---
+++ src/main/java/com/example/rockpedia/RockpediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rockpedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockpediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockpediaApplication.class, args);
+	}
+
+}
