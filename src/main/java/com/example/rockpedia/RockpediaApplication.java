33cef10a28bf4f6533fd64acd03eaca2e560f779
Arnaud Couderc <costar57@gmail.com>
2020-01-09 23:30:37 +0100

    :chart_with_upwards_trend: added spring actuator and prometheus

--- src/main/java/com/example/rockpedia/RockpediaApplication.java
+++ src/main/java/com/example/rockpedia/RockpediaApplication.java
@@ -1,8 +1,9 @@
 package com.example.rockpedia;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 public class RockpediaApplication {
