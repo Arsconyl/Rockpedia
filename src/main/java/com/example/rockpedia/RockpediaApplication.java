1c6b31468a44c3a1bbf7d15d4622c5ef3b13c7ef
Arnaud Couderc <costar57@gmail.com>
2020-01-13 17:15:11 +0100

    :chart_with_upwards_trend: added params and dependencies for Spring Boot Admin

--- src/main/java/com/example/rockpedia/RockpediaApplication.java
+++ src/main/java/com/example/rockpedia/RockpediaApplication.java
@@ -3,11 +3,22 @@ package com.example.rockpedia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @SpringBootApplication
 public class RockpediaApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RockpediaApplication.class, args);
+	    SpringApplication.run(RockpediaApplication.class, args);
+	}
+
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
 	}
 }
